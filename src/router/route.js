c1e1a65247bb78bc1dba248ca23a0b0ee4e09ccc
NOH <01023571995@gmail.com>
2020-08-20 18:03:12 +0900

    Project Frame Updated

--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/views/layout/Main'
+import chartTemplate from '@/views/guide/chartTemplate'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/Main',
       name: 'Main',
       component: Main
+    },
+    {
+      path: '/guide',
+      name: 'chartTemplate',
+      component: chartTemplate
     }
   ]
 })
---
+++ src/views/guide/chartTemplate.vue
@@ -0,0 +1,16 @@
+<template>
+  <LineChart></LineChart>
+</template>
+
+<script>
+export default {
+  name: 'chartTemplate',
+  components: {
+    LineChart: () => import('../../components/chart/LineChart')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
