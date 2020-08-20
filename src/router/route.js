5ce97271649a997ccc6c890e70862bdfd46c2a15
NOH <01023571995@gmail.com>
2020-08-20 17:29:20 +0900

    Project Frame Updated

--- src/router/route.js
+++ src/router/route.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/views/layout/Main'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/Main',
+      name: 'Main',
+      component: Main
     }
   ]
 })
